cf4cd09b090676538ca2e87f18602ef785198875
haoxin <coderhaoxin@outlook.com>
2016-06-07 16:03:59 +0800

    rm: old

--- old/config/index.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-const env = process.env.NODE_ENV || 'development'
-
-module.exports = require('./' + env)
--- old/static/pc/entry/index.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-import { Hello } from '../component/hello'
-
-window.init = () => {
-  React.render(<Hello></Hello>, document.body)
-}
