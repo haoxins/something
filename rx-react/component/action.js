b9821f0296667aa5771f4a292e030652f98a4b48
haoxin <haoxins@outlook.com>
2017-01-22 17:26:21 +0800

    remove rx-react; move to coderhaoxin/react.rx

--- rx-react/component/action.js
+++
@@ -1,7 +0,0 @@
-
-import rx from 'rxjs'
-import { createAction } from 'react.rx'
-
-export const switchItem = createAction(function(id) {
-  return id
-})
--- rx-react/index.js
+++
@@ -1,5 +0,0 @@
-
-import { render } from 'react-dom'
-import App from './component/app'
-
-render(<App />, document.querySelector('body > main'))
