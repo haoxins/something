7d27e3f8067e2dedbf3e6be25ea6cc9500e7ef29
haoxin <haoxins@outlook.com>
2017-10-12 22:27:08 +0800

    update

--- hello-angular/src/app/app.component.ts
+++ hello-angular/src/app/app.component.ts
@@ -1,4 +1,14 @@
+
 import { Component } from '@angular/core'
+import { Hero } from './hero'
+
+const HEROES: Hero[] = [{
+  id: 1, name: '虫虫'
+}, {
+  id: 2, name: '棒棒'
+}, {
+  id: 3, name: '毛毛'
+}]
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,12 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app'
+  title = '哇塞'
+
+  heroes = HEROES
+  selectedHero: Hero
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero
+  }
 }
--- hello-angular/src/app/app.module.ts
+++ hello-angular/src/app/app.module.ts
@@ -1,16 +1,23 @@
+
 import { BrowserModule } from '@angular/platform-browser'
+import { FormsModule } from '@angular/forms'
 import { NgModule } from '@angular/core'
 
+import { HeroDetailComponent } from './hero-detail.component'
 import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [
+    HeroDetailComponent,
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
